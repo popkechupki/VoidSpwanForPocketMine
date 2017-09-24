c750b6047e15e9daf9a784ab1f020813d10696f8
こもれび / popkechupki <hobbypopkechupki@gmail.com>
2017-09-24 19:29:54 +0900

    Create VoidSpawn.php

---
+++ VoidSpawn/src/popkechupki/VoidSpawn.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace popkechupki;
+
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityDamageEvent;
+
+class VoidSpawn extends PluginBase implements Listener {
+  public function onEnable() {
+    $this->getServer()->getPluginManager()->registerEvents($this,$this);
+  }
+  public function onEntityDamage(EntityDamageEvent event) {
+    if($event->getEntity() instanceof Player) {
+      switch($event->getCause()) {
+        case VOID:
+          $event->setCancelled();
+          $event->getEntity()->teleport($event->getEntity()->getLevel()->getSafeSpawn());
+      }
+    }
+  }
+}
